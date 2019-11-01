39f0de95cb43631b71bc65e85c25d27f68165906
Eric Greene <eric@t4d.io>
2019-11-01 10:43:04 -0700

    add code demos

--- apollo-graphql-app-color-tool/src/App.js
+++ apollo-graphql-app-color-tool/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { useQuery, /* Query */ } from 'react-apollo';
+import { useQuery, Query, useMutation } from 'react-apollo';
 import gql from 'graphql-tag';
 
 import { ToolHeader } from './components/ToolHeader';
 import { UnorderedList } from './components/UnorderedList';
+import { ColorForm } from './components/ColorForm';
 
 const APP_QUERY = gql`
   query AppQuery {
     colors {
+      id
+      name
+      hexcode
+    }
+  }
+`;
+
+const APPEND_COLOR_MUTATION = gql`
+  mutation AppendColor($color: AppendColor) {
+    appendColor(color: $color) {
       id
       name
       hexcode
@@ -20,17 +31,51 @@ export const App = () => {
   
   const { loading, data, error } = useQuery(APP_QUERY);
 
-  if (loading) {
-    return <div>Loading!</div>;
-  }
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
+  const [ mutateAppendColor ] = useMutation(APPEND_COLOR_MUTATION);
+
+  const appendColor = color => {
+
+    return mutateAppendColor({
+      variables: {
+        color: {
+          ...color,
+        },
+      },
+      // refetchQueries: [ { query: APP_QUERY } ],
+      // optimisticResponse: {
+      //   appendColor: {
+      //     ...color,
+      //     id: -1,
+      //     __typename: 'Color',
+      //   },
+      // },
+      // cache-first, notified of server changes, use a subscription
+      update(store, { data: { appendColor: color }}) {
+
+        console.log('update', color);
+
+        const data = store.readQuery({ query: APP_QUERY });
+
+        const newData = {
+          ...data,
+          colors: data.colors.concat(color),
+        };
+
+        store.writeQuery({ query: APP_QUERY, data: newData });
+      }
+    });
+
+  };
+
+  if (loading) return <div>Loading!</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return <>
     <ToolHeader headerText="Color Tool" />
     <UnorderedList items={data.colors}>
-      {(item) => item.name + ' - ' + item.hexcode}
+      {(item) => item.id + ': ' + item.name + ' - ' + item.hexcode}
     </UnorderedList>
+    <ColorForm buttonText="Add Color" onSubmitColor={appendColor} />
   </>;
 };
 
